66cf6af3500765cbd5a2a96dd39de677514c0e37
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-11-04 23:55:53 +0300

    add models

--- errors/index.js
+++
@@ -1,8 +0,0 @@
-const BadRequestError = require('./bad-request-error');
-const NotFoundError = require('./not-found-error');
-const UnauthorizedError = require('./unauthorized-error');
-const ForbiddenError = require('./forbidden-error');
-
-module.exports = {
-  BadRequestError, NotFoundError, UnauthorizedError, ForbiddenError,
-};
--- models/user.js
+++ models/user.js
@@ -1,5 +1,54 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/unauthorized-error');
+
 const userSchema = new mongoose.Schema({
   name: {
-    
+    type: String,
+    required: [true, 'Длина поля Имени должна быть от 2 до 30 символов'],
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(name) {
+        return !validator.isEmpty(name, { ignore_whitespace: true });
+      },
+      message: 'Введенo некорректное Имя',
+    },
+  },
+  email: {
+    type: String,
+    required: [true, 'Необходимо указать email'],
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: 'Введён некорректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
   }
 })
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError({ message: 'Неправильные email или пароль' });
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError({ message: 'Неправильные email или пароль' });
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
